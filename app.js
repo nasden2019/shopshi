363531f1e7a9f48e0ce84528f26790a0a9db5e81
Alex <alex@syloper.com>
2020-07-30 18:42:53 -0300

    first update

---
+++ app.js
@@ -0,0 +1,19 @@
+const chk = document.getElementById("chk");
+const text = document.querySelector(".turnOff");
+
+chk.addEventListener("click", () => {
+  // document.body.style.background = 'black';
+  document.body.classList.toggle("dark");
+  document.text.classList.toggle("white");
+});
+
+// to change Text:
+chk.addEventListener("click", changeText);
+
+function changeText() {
+  if (text.innerHTML == "Turn On the Day") {
+    text.innerHTML = "Turn it off!";
+  } else {
+    text.innerHTML = "Turn On the Day";
+  }
+}
